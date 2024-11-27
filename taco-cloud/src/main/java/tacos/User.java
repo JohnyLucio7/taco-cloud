29237e1d41c8abd8aed3b3cb24958c2e2c3e8d11
Johny Lucio <johnylucio7@gmail.com>
2024-11-27 17:46:07 -0300

    [Part 1 - Foundational Spring] - 5 Securing Spring
    5.2 Configuring authentication
    	5.2.1 In-memory user details service
    	5.2.2 Customizing user authentication
    5.3 Securing web requests
    	5.3.1 Securing requests
    	5.3.2 Creating a custom login page
    	5.3.3 Enabling third-party authentication
    	5.3.4 Preventing cross-site request forgery

---
+++ taco-cloud/src/main/java/tacos/User.java
@@ -0,0 +1,57 @@
+package tacos;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+
+	private String username;
+	private String password;
+	private String fullname;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+}
